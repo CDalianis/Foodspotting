4bcb93f2d0b997265be506719e7d0192585b8312
CDalianis <christos_dl@hotmail.com>
2026-06-06 13:16:40 +0300

    feat add: RoleRepo init commit

---
+++ src/main/java/gr/projectfoodspots/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package gr.projectfoodspots.repository;
+
+import gr.projectfoodspots.model.Role;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+}
