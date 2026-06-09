456b9277f69b4a71f366b5a05b1e8596cbc410cf
CDalianis <christos_dl@hotmail.com>
2026-06-09 20:44:03 +0300

    feat add: PlrojectfoodspotsApplication init commit

---
+++ src/main/java/gr/projectfoodspots/ProjectfoodspotsApplication.java
@@ -0,0 +1,13 @@
+package gr.projectfoodspots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectfoodspotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectfoodspotsApplication.class, args);
+	}
+
+}
